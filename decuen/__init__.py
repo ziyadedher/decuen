d4adde9b69db6a0181f497ad6371e50c4c2b17a4
Ziyad Edher <ziyad.edher@gmail.com>
2019-10-28 19:41:56 -0400

    Create basic project structure and setup

---
+++ decuen/__init__.py
@@ -0,0 +1,4 @@
+"""Reinforcement learning framework for experimentation, ease-of-use, modularity, and extensibility.
+
+Built in modern Python with full type annotations.
+"""
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""Setup decuen installation.
+
+Uses setup.cfg to configure installation.
+"""
+
+from setuptools import setup  # noqa
+
+if __name__ == '__main__':
+    setup()
