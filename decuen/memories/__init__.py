3f2240c0b40082a6a0ac64b8a11bf2d110fd8970
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-05 17:47:13 -0500

    Generate module-level imports for remaining modules

--- decuen/memories/__init__.py
+++ decuen/memories/__init__.py
@@ -1,1 +1,6 @@
 """Memory manager interfaces and implementations for different techniques of experience replay."""
+
+from decuen.memories._memory import Memory, Trajectory, Transition
+from decuen.memories.short_term import ShortTermMemory
+
+__all__ = ["Transition", "Trajectory", "Memory", "ShortTermMemory"]
--- decuen/policies/__init__.py
+++ decuen/policies/__init__.py
@@ -1,1 +1,5 @@
 """Policy interfaces and implementations for different methods of policy use, generation, and adaptation."""
+
+from decuen.policies._policy import Policy
+
+__all__ = ["Policy"]
