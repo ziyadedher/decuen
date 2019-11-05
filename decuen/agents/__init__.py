2cc8f0efd4043ce5a161adc568ceaf6abe39182f
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-05 17:35:13 -0500

    Generalize actor-critic agent interface to support more configurations

--- decuen/agents/__init__.py
+++ decuen/agents/__init__.py
@@ -1,5 +1,5 @@
 """High-level agent interfaces for reinforcement learning environments."""
 
-from decuen.agents._agent import Agent, AgentSettings
+from decuen.agents._agent import ActorAgent, Agent, AgentSettings, CriticAgent
 
-__all__ = ["Agent", "AgentSettings"]
+__all__ = ["Agent", "AgentSettings", "ActorAgent", "CriticAgent"]
