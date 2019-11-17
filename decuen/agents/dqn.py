54b3f3f6afbc6930bbb128d0025991ab4ef11884
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-17 09:31:58 -0500

    Implement DDQN

--- decuen/agents/dqn.py
+++ decuen/agents/dqn.py
@@ -5,30 +5,29 @@ from dataclasses import dataclass
 
 import numpy as np  # type: ignore
 from gym.spaces import Discrete  # type: ignore
-from tensorflow.keras import Sequential  # type: ignore
 
 from decuen.agents._agent import AgentSettings, CriticAgent
-from decuen.critics.dqn import DQNCritic, DQNCriticSettings
+from decuen.critics.dqn import _DQNCritic
 from decuen.memories._memory import Memory
 from decuen.strategies._strategy import Strategy
 
 
 @dataclass
-class DQNAgentSettings(AgentSettings, DQNCriticSettings):
+class DQNAgentSettings(AgentSettings):
     """Settings for deep Q-network critic based agents."""
 
 
 class DQNAgent(CriticAgent):
     """Critic agent with only a deep Q-network based critic guiding action selection."""
 
-    critic: DQNCritic
+    critic: _DQNCritic
 
     # pylint: disable=too-many-arguments
-    def __init__(self, state_space: Discrete, action_space: Discrete, settings: DQNAgentSettings,
-                 memory: Memory, strategy: Strategy, model: Sequential) -> None:
+    def __init__(self, state_space: Discrete, action_space: Discrete,
+                 memory: Memory, critic: _DQNCritic, strategy: Strategy,
+                 settings: DQNAgentSettings = DQNAgentSettings()) -> None:
         """Initialize a deep Q-network critic agent."""
-        critic = DQNCritic(state_space, action_space, settings, model)
-        super().__init__(state_space, action_space, settings, memory, critic, strategy)
+        super().__init__(state_space, action_space, memory, critic, strategy, settings)
 
     def act(self, state: np.ndarray) -> np.ndarray:
         """Generate an action to perform based on the Q-values of different actions in a state."""
