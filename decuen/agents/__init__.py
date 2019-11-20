5375c506ffd2e83f201491a8ec8ad3bce6a4280e
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-19 20:55:08 -0500

    Refactor and simplify Q-critic based agents

--- decuen/agents/__init__.py
+++ decuen/agents/__init__.py
@@ -1,11 +1,9 @@
 """High-level agent interfaces and implementations for reinforcement learning environments."""
 
 from decuen.agents._agent import ActorAgent, Agent, AgentSettings, CriticAgent
-from decuen.agents.dqn import DQNAgent, DQNAgentSettings
-from decuen.agents.qtable import QTableAgent, QTableAgentSettings
+from decuen.agents.q import QAgent, QAgentSettings
 
 __all__ = [
     "Agent", "AgentSettings", "ActorAgent", "CriticAgent",
-    "DQNAgent", "DQNAgentSettings",
-    "QTableAgent", "QTableAgentSettings",
+    "QAgent", "QAgentSettings",
 ]
