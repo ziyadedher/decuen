485460b56bca5c2857959d336ab6830b40fbc625
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-20 13:43:03 -0500

    Migrate DQN implementation to PyTorch

--- decuen/critics/dqn.py
+++ decuen/critics/dqn.py
@@ -10,22 +10,27 @@ Implements both deep Q-learning [1, 2] and double deep Q-learning [3] algorithms
     https://arxiv.org/pdf/1509.06461.pdf
 """
 
+import copy
 from dataclasses import dataclass
 from typing import MutableSequence
 
 import numpy as np  # type: ignore
 from gym.spaces import Discrete  # type: ignore
-from tensorflow.keras import Sequential  # type: ignore
-from tensorflow.keras.models import clone_model  # type: ignore
+from torch import tensor, zeros  # pylint: disable=no-name-in-module
+from torch.nn import Module
+from torch.optim import Optimizer  # type: ignore
 
 from decuen.critics._q import QCritic, QCriticSettings
 from decuen.memories._memory import Transition
-from decuen.utils import checks
 
 
 @dataclass
 class DQNCriticSettings(QCriticSettings):
     """Settings for Q-table critics."""
+
+    clipped: bool
+    optimizer: Optimizer
+    loss: Module
 
 
 class DQNCritic(QCritic):
@@ -36,10 +41,10 @@ class DQNCritic(QCritic):
 
     action_space: Discrete
     settings: DQNCriticSettings
-    network: Sequential
-    _target_network: Sequential
+    network: Module
+    _target_network: Module
 
-    def __init__(self, model: Sequential, settings: DQNCriticSettings) -> None:
+    def __init__(self, model: Module, settings: DQNCriticSettings) -> None:
         """Initialize this generic actor critic interface."""
         super().__init__(settings)
 
@@ -48,48 +53,53 @@ class DQNCritic(QCritic):
             raise TypeError("action space for Q-table critic must be discrete")
 
         self.network = model
-        self._target_network = clone_model(model)
-        self._target_network.set_weights(self.network.get_weights())
+        self._target_network = copy.deepcopy(model)
+        self._target_network.eval()
 
     def learn(self, transitions: MutableSequence[Transition]) -> None:
         """Update internal critic representation based on past transitions."""
         self._learn_step += 1
-
         if not transitions:
             return
-        for transition in transitions:
-            checks.check_transition(self.state_space, self.action_space, transition)
 
-        states = np.array([transition.state for transition in transitions])
-        new_states = np.array([transition.new_state for transition in transitions])
+        states = tensor([np.atleast_1d(transition.state)
+                         for transition in transitions])
+        actions = tensor([np.atleast_1d(transition.action)
+                         for transition in transitions])
+        new_states_not_terminal = tensor([np.atleast_1d(transition.new_state)
+                                          for transition in transitions
+                                          if not transition.terminal])
+        rewards = tensor([transition.reward
+                          for transition in transitions])
+        not_terminal = tensor([not transition.terminal
+                               for transition in transitions])
 
-        values = self.network.predict_on_batch(states).numpy()
-        new_values = self.network.predict_on_batch(new_states).numpy()
-        target_new_values = self._target_network.predict_on_batch(new_states)
+        values = self.network(states).gather(1, actions)
+        next_values = zeros(len(transitions))
+        if self.settings.double:
+            chosen_actions = self._target_network(new_states_not_terminal).argmax(1, keepdims=True)
+            next_values[not_terminal] = (self.network(new_states_not_terminal)
+                                         .gather(1, chosen_actions).squeeze(1).detach())
+        else:
+            next_values[not_terminal] = self._target_network(new_states_not_terminal).max(1)[0].detach()
+        target_values = (rewards + (self.settings.discount_factor * next_values)).unsqueeze(1)
 
-        for i, transition in enumerate(transitions):
-            target = transition.reward
-            if not transition.terminal:
-                if self.settings.double:
-                    target += self.settings.discount_factor * new_values[i][np.argmax(target_new_values[i])]
-                else:
-                    target += self.settings.discount_factor * np.max(target_new_values[i])
-            values[i][transition.action] = target
-
-        self.network.train_on_batch(states, values)
+        loss = self.settings.loss(values, target_values)
+        self.settings.optimizer.zero_grad()
+        loss.backward()
+        if self.settings.clipped:
+            for param in self.network.parameters():
+                if param.grad is not None:
+                    param.grad.data.clamp_(-1, 1)
+        self.settings.optimizer.step()
 
         if self._learn_step % self.settings.target_update == 0:
-            self._target_network.set_weights(self.network.get_weights())
+            self._target_network.load_state_dict(self.network.state_dict())
 
     def crit(self, state: np.ndarray, action: np.ndarray) -> float:
         """Return the Q-value of taking a specific action in a specific state."""
-        checks.check_state(self.state_space, state)
-        checks.check_action(self.action_space, action)
-
         return self.values(state)[action]
 
     def values(self, state: np.ndarray) -> np.ndarray:
         """Return an array of Q-values of all actions in a specific state."""
-        checks.check_state(self.state_space, state)
-
-        return self.network.predict_on_batch(np.array([state]))[0].numpy()
+        return self.network(tensor([state])).detach()[0]
