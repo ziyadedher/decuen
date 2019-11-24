5ece246c5953f541957c84dac012913301992d89
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-24 18:13:59 -0500

    Include policy-gradient actor in init

--- decuen/actors/__init__.py
+++ decuen/actors/__init__.py
@@ -1,9 +1,11 @@
 """Actor-learner interfaces and implementations for generating and learning behavioral policies."""
 
 from decuen.actors._actor import Actor, ActorSettings
+from decuen.actors.pg import PGActor, PGActorSettings
 from decuen.actors.strategy import StrategyActor
 
 __all__ = [
     "Actor", "ActorSettings",
     "StrategyActor",
+    "PGActor", "PGActorSettings"
 ]
