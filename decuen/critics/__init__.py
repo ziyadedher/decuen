d1f2ac3d1c945df28cc58fccc38d951f384ba076
Ziyad Edher <ziyad.edher@gmail.com>
2019-11-26 11:41:09 -0500

    Implement Monte Carlo-based expected reward estimation critic

--- decuen/critics/__init__.py
+++ decuen/critics/__init__.py
@@ -1,13 +1,20 @@
 """Critic interfaces and implementations for generating and learning critical analysis of actions and states."""
 
-from decuen.critics._critic import (ActionCritic, ActionCriticSettings, Critic,
-                                    CriticSettings, StateCritic,
-                                    StateCriticSettings)
+from typing import Union
+
+from decuen.critics._critic import (ActionValueCritic, AdvantageCritic,
+                                    CriticSettings, StateValueCritic)
 from decuen.critics.dqn import DQNCritic, DQNCriticSettings
+from decuen.critics.montecarlo import (MonteCarloCritic,
+                                       MonteCarloCriticSettings)
+
+Critic = Union[ActionValueCritic, StateValueCritic, AdvantageCritic]
 
 __all__ = [
     "Critic", "CriticSettings",
-    "StateCritic", "StateCriticSettings",
-    "ActionCritic", "ActionCriticSettings",
+    "StateValueCritic",
+    "ActionValueCritic",
+    "AdvantageCritic",
     "DQNCritic", "DQNCriticSettings",
+    "MonteCarloCritic", "MonteCarloCriticSettings",
 ]
---
+++ decuen/critics/montecarlo.py
@@ -0,0 +1,40 @@
+"""Implementation of a rudimentary Monte Carlo "reward" critic.
+
+Simply estimates the state values based on a Monte Carlo estimate of the expected reward.
+"""
+
+from dataclasses import dataclass
+from typing import MutableSequence, Union
+
+from torch import arange
+
+from decuen.critics._critic import AdvantageCritic, CriticSettings
+from decuen.structs import (Tensor, Trajectory, Transition, batch_transitions,
+                            tensor)
+
+
+@dataclass
+class MonteCarloCriticSettings(CriticSettings):
+    """Settings for Monte Carlo critics."""
+
+
+class MonteCarloCritic(AdvantageCritic):
+    """Monte Carlo critic."""
+
+    def __init__(self, settings: MonteCarloCriticSettings) -> None:
+        """Initialize a Monte Carlo critic."""
+        super().__init__(settings)
+
+    def learn(self, transitions: MutableSequence[Transition]) -> None:
+        """Do nothing. Monte Carlo critic does not learn."""
+
+    def advantage(self, trajectory: Union[Transition, Trajectory]) -> Tensor:
+        """Estimate the advantage of every transition in a trajectory using a Monte Carlo method.
+
+        Note that this advantage estimate tends to have high variance, even when using a baseline.
+        """
+        batch = batch_transitions([trajectory] if isinstance(trajectory, Transition) else trajectory)
+        discounted_rewards = tensor([self.settings.discount_factor]).pow(arange(batch.rewards.size()[0]))
+        advantages = discounted_rewards.flip(0).cumsum(0).flip(0)  # Reverse cumulative sum (causality)
+        print(advantages)
+        return advantages
