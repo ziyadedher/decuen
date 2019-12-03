549ffb9edac45acd80a5a36c9d06b7d995714203
Ziyad Edher <ziyad.edher@gmail.com>
2019-12-02 21:55:00 -0500

    Abstract away torch tensor to workaround pylint issue

---
+++ decuen/structs/tensor.py
@@ -0,0 +1,14 @@
+"""Workaround for the 'tensor is not callable' pylint error.
+
+Simply contains a forwarder of the torch tensor function that forces the linter to agree that the function is, in fact,
+a function.
+"""
+
+import torch
+
+
+def tensor(*args, **kwargs) -> torch.Tensor:  # noqa
+    return torch.tensor(*args, **kwargs)  # noqa
+
+
+tensor.__doc__ = torch.tensor.__doc__
