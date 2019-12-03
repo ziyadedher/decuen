85ca08bed7d727f895f0fab1b8f227eda5a9c6a6
Ziyad Edher <ziyad.edher@gmail.com>
2019-12-02 21:55:19 -0500

    Pull out transition structure into its own file

---
+++ decuen/structs/transition.py
@@ -0,0 +1,29 @@
+"""Constructs representing and helpers for transitions in the framework."""
+
+from dataclasses import dataclass
+from typing import Optional
+
+from decuen.dists import Distribution
+from decuen.structs.action import Action
+from decuen.structs.state import State
+
+
+@dataclass
+class Transition:
+    """Representation of a transition.
+
+    A transition is minimally defined by an initial state, an action taken on that state, and the resultant state. In
+    the context of finite-horizon reinforcement learning, we also require two additional pieces of data: a reward
+    attained due to taking that action in that state and whether or not this transition ends in a terminal state, i.e.
+    one in which the simulation ends.
+    """
+
+    state: State
+    action: Action
+    new_state: State
+    reward: float
+    terminal: bool
+
+    behavior: Optional[Distribution] = None
+    state_value: Optional[float] = None
+    action_value: Optional[float] = None
