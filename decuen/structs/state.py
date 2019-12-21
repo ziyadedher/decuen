916be9b4a2befa59c2c4e86e3cf8c3613aa043d4
Ziyad Edher <ziyad.edher@gmail.com>
2019-12-20 20:57:51 -0500

    Introduce contextfulness to actions and states

--- decuen/structs/state.py
+++ decuen/structs/state.py
@@ -1,11 +1,16 @@
 """Constructs representing and helpers for states (which we conflate with observations) in the framework."""
 
+from typing import Union
+
 import numpy as np  # type: ignore
+from gym.spaces import Discrete  # type: ignore
 from torch import Tensor, from_numpy
+
+from decuen.utils.context import Contextful
 
 
 # TODO: hook in state checking here?
-class State:
+class State(Contextful):
     """State (observation) representation.
 
     Essentially a thin wrapper around some data and provides hooks to access either the array data or the tensor data.
@@ -13,9 +18,12 @@ class State:
 
     _data: Tensor
 
-    def __init__(self, data: np.ndarray) -> None:
+    def __init__(self, data: Union[np.ndarray, float, int]) -> None:
         """Initialize a state."""
-        self._data = from_numpy(data).float()
+        super().__init__()
+        self._data = from_numpy(np.array(data))
+        if not isinstance(self.state_space, Discrete):
+            self._data = self._data.float()
 
     @property
     def numpy(self) -> np.ndarray:
